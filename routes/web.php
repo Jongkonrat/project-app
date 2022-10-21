d146bd78ca297c252990b7ad18b467e644ad166f
minmeeen <sirapat.mp@gmail.com>
2022-10-21 15:39:28 +0700

    debug $products successfully

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::group(['namespace'=> 'App\Http\Controllers'], function()
      */
     Route::get('/', 'ProductController@index')->name('home.index');
     
+<<<<<<< Updated upstream
     // Route::group(['middleware'=> ['guest']], function() {
     //     /**
     //      * Register Routes
@@ -35,6 +36,25 @@ Route::group(['namespace'=> 'App\Http\Controllers'], function()
     //       Route::get('/login', 'LoginController@show')->name('login.show');
     //       Route::post('/login', 'LoginController@login')->name('login.perform');
     //     });
+=======
+    Route::group(['middleware'=> ['guest']], function() {
+        /*** Home Routes
+     */
+      Route::get('/', 'ProductController@index')->name('home.index');
+
+      
+        /** 
+          * Register Routes
+         */
+           Route::get('/register', 'RegisterController@show')->name('register.show');
+           Route::post('/register', 'RegisterController@register')->name('register.perform');
+          /**
+           * Login Routes
+           */
+          Route::get('/login', 'LoginController@show')->name('login.show');
+          Route::post('/login', 'LoginController@login')->name('login.perform');
+        });
+>>>>>>> Stashed changes
         
     //     Route::group(['middleware'=> ['auth']], function() {
     //         /**
