8b3e073a33ff75909318f7ff34c0b22c713e3d5e
Jongkonrat <76035221+Jongkonrat@users.noreply.github.com>
2022-11-05 23:26:07 +0700

    add filable

--- app/Models/contact.php
+++ app/Models/contact.php
@@ -5,7 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class contact extends Model
+class Contact extends Model
 {
     use HasFactory;
+    protected $table = 'offices';
+    protected $fillable = [
+        'officeCode',
+        'city',
+        'phone',
+        'addressLine1',
+        'addressLine2',
+        'state',
+        'country',
+        'postalCode',
+        'territory'
+    ];
 }
