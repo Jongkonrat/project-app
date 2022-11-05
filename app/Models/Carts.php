4b85dc15b0fc9178b75aa6a807ed92b62a3c9301
ploylapas <lapasrada_k@cmu.ac.th>
2022-11-05 20:12:45 +0700

    add cart model

---
+++ app/Models/Carts.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Carts extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customerNumber', 'productCode', 'quantity'
+    ];
+}
