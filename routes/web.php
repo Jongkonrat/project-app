168cf25e6b3462f0fac864509c63be19a85e80f2
ploylapas <lapasrada_k@cmu.ac.th>
2022-11-05 23:44:04 +0700

    edit path

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::group(['namespace'=> 'App\Http\Controllers'], function()
     /*** Home Routes
      */
 
+    Route::get('/', 'ProductController@index')->name('home.index');
     Route::get('/home', 'ProductController@index')->name('home.index');
 
     // Route::get('/', 'HomeController@index')->name('home.index');
