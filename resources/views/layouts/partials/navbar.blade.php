8caad04e5eba6b220fe745a127e0619b7a96752d
ploylapas <lapasrada_k@cmu.ac.th>
2022-11-06 05:54:35 +0700

    add button profile

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <header class="p-3 bg-dark text-white">
     <div class="container d-flex flex-wrap">
-        <div class="d-flex flex-wrap align-items-center justify-content-start justify-content-lg-start" style= "width: fit-content;">
+        <div class="d-flex flex-wrap align-items-center justify-content-start justify-content-lg-start" style="width: fit-content;">
             <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
                 <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
                     <use xlink:href="#bootstrap" />
@@ -18,17 +18,20 @@
                 <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
             </form> -->
             @auth
-            
+
             <a href="{{ url('cart') }}" style="border-radius: 5px; padding: 7px; padding-left: 16px; padding-right: 16px;
-            background-color: yellow; margin-right: 12px;">Cart</a>
-            
+            background-color: rgb(255, 169, 15); margin-right: 12px;">Cart</a>
+
+            <a href="{{ url('profile') }}" style="border-radius: 5px; padding: 7px; padding-left: 16px; padding-right: 16px;
+            background-color: white; margin-right: 12px;">Profile</a>
+
             {{auth()->user()->name}}
 
             <!-- <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
                 <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
             </form> -->
-            <div class="text-end" style="margin-left: 75px;">
-                <a href="{{ route('logout.perform') }}" class="btn btn-outline-light me-2" style = "align-items: flex-end;">Logout</a>
+            <div class="text-end" ">
+                <a href="{{ route('logout.perform') }}" class="btn btn-outline-light me-2" style="align-items: flex-end;">Logout</a>
             </div>
             @endauth
             @guest
