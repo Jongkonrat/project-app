6caa83ade4801e3da7285cc120440d334a7050cb
kkob999 <kob.ruji@gmail.com>
2022-11-06 08:52:40 +0700

    cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app-master')
 
 @section('content')
-<div style="margin-top: 7%; background-color: white; border-radius: 10px; padding: 18px; width: fit-content; 
-box-shadow: 0px 10px 30px 5px rgb(0,0,0,.15)">
-<table >
+<div style="background-color: white; border-radius: 10px; padding: 18px; width: fit-content; 
+box-shadow: 0px 10px 30px 5px rgb(0,0,0,.15); width:70%; margin: auto; margin-top: 7%;">
+<table class="border-collapse align-items-center" style="justify-item: center;">
     @auth
-    <thead style="text-align: center; border-bottom: dashed black;">
+    <thead class="border-collapse" style="text-align: center; border-bottom: dashed grey; margin: 40px;">
         <tr>
             <th style="padding-bottom: 1.2%;">Product</th>
             <th style="padding-bottom: 1.2%; width: 30%;">Name</th>
-            <th style="padding-bottom: 1.2%;">Price</th>
+            <th style="padding-bottom: 1.2%; padding-right: 20px; padding-left: 20px;">Price</th>
             <th style="padding-bottom: 1.2%;">Quantity</th>
-            <th style="padding-bottom: 1.2%;"class="text-center">Subtotal</th>
-            <th style="padding-bottom: 1.2%;"></th>
+            <th style="padding-bottom: 1.2%; padding-right: 20px; padding-left: 20px;"class="text-center">Subtotal</th>
+            <th style="padding-bottom: 1.2%; "></th>
         </tr>
     </thead>
-    <tbody style="">
+    <tbody style="padding: 10px;">
     @php 
     $total = 0 ;
     $id = auth()->user()->id;
@@ -25,16 +25,21 @@ box-shadow: 0px 10px 30px 5px rgb(0,0,0,.15)">
             
             @if($cart -> customerNumber == $id)
                 <tr>
-                    <td class="inner-table" style="text-align: center;">{{ $cart -> productCode  }}</td>
-                    <td class="inner-table" style="">{{ $cart -> productName  }}</td>
-                    <td class="inner-table" style="text-align: center;">{{ $cart->buyPrice }}</td>
-                    <td class="inner-table" style="text-align: center;">{{ $cart->quantity }}
-                    <td data-th="Subtotal" class="text-center" style="">{{$cart->buyPrice*$cart->quantity}}</td>
-                    <td style ="justify-content: flex-end; padding-top: 5px;">
+                    <td class="inner-table" style="text-align: center; padding-right: 20px; 
+                    padding-left: 20px; padding-top: 10px; padding-bottom: 10px;">{{ $cart -> productCode  }}</td>
+                    <td class="inner-table" style="padding-right: 20px; padding-left: 20px;
+                    padding-top: 10px; padding-bottom: 10px;">{{ $cart -> productName  }}</td>
+                    <td class="inner-table" style="text-align: center; padding-right: 20px; padding-left: 20px;
+                    padding-top: 10px; padding-bottom: 10px;">{{ $cart->buyPrice }}</td>
+                    <td class="inner-table" style="text-align: center; padding-right: 20px; padding-left: 20px;
+                    padding-top: 10px; padding-bottom: 10px;">{{ $cart->quantity }}
+                    <td data-th="Subtotal" class="text-center" style="padding-right: 20px; padding-left: 20px;
+                    padding-top: 10px; padding-bottom: 10px;">{{$cart->buyPrice*$cart->quantity}}</td>
+                    <td style ="">
                         <form action="{{ url('remove-from-cart/'.$cart->id) }}" method="POST">
                             @csrf
                             @method('DELETE')
-                            <button class="btnbtn-danger" ><i class="fa fa-trash-o"></i></button>
+                            <button class="btnbtn-danger" "><i class="fa fa-trash-o"></i></button>
                         </form>
                     </td>
                 </tr>
@@ -43,13 +48,15 @@ box-shadow: 0px 10px 30px 5px rgb(0,0,0,.15)">
 
         @endforeach
     </tbody>
-    <tfoot style="border-top:dashed black; margin-top: 20px;">
+    <tfoot style="border-top:dashed grey; margin-top: 20px;">
         <tr style="">
-            <td colspan="6"class="text-center" style="padding-top: 10px;">
+            <td colspan="4"class="text-center" style="padding-top: 20px;">
                 <a href="{{ url('/') }}"class="btnbtn-warning" style="margin-left: 100px"><iclass="fa fa-angle-left"></i>Continue Shopping</a>
                 <button class="btn btn-success" style="margin-left: 30px">Checkout</button>
             </td>
-            <td colspan="6" style="text-align: end; padding-top: 10px;"><h3><strong>Total {{ $total }}<h3><strong></td>
+            <td colspan="4" style="padding-top: 20px;">
+                <h3><strong>Total {{ $total }} $<h3><strong>
+            </td>
         </tr>
         
     </tfoot>
