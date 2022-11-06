3caaee12a858afa5e9c0590a71d77e5c185d1c4e
Jongkonrat <76035221+Jongkonrat@users.noreply.github.com>
2022-11-06 08:41:11 +0700

    update profile

--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -5,19 +5,36 @@
     @php
     $id = auth()->user()->id;
     @endphp
+    <div style="padding-top: 50px">
+        <div style="padding-left: 300px">
+            <img src="https://www.techhub.in.th/wp-content/uploads/2021/05/577280151-1.jpg" alt="">
+            <div style="padding-left: 150px">
+                @foreach($customers as $customer)
+                    @if($customer -> customerNumber == $id)
+                        <h1>{{ $customer->customerName }}</h1>
+                        <p>ID: {{ $customer->customerNumber }}</p>
+                        <p>First Name: {{$customer->contactFirstName}}</p>
+                        <p>Last Name: {{$customer->contactLastName}}</p>
+                        <p>Phone: {{$customer->phone}}</p>
+                        <p>Address: {{$customer->addressLine1}}</p>
+                        <p>City: {{$customer->city}}</p>
+                        <p>State: {{$customer->state}}</p>
+                        <p>Postal Code: {{$customer->postalCode}}</p>
+                        <p>Country: {{$customer->country}}</p>
+                    @endif
+                @endforeach
+            </div>
+        </div>
+    </div>
+    <style>
+        h1{
+        color: #000;
+        font-family: verdana;
+    }
 
-    @foreach($customers as $customer)
-        @if($customer -> customerNumber == $id)
-            <h1>{{ $customer->customerName }}</h1>
-            <p>ID: {{ $customer->customerNumber }}</p>
-            <p>First Name: {{$customer->contactFirstName}}</p>
-            <p>Last Name: {{$customer->contactLastName}}</p>
-            <p>Phone: {{$customer->phone}}</p>
-            <p>Address: {{$customer->addressLine1}}</p>
-            <p>City: {{$customer->city}}</p>
-            <p>State: {{$customer->state}}</p>
-            <p>Postal Code: {{$customer->postalCode}}</p>
-            <p>Country: {{$customer->country}}</p>
-        @endif
-    @endforeach
+    p{
+        color: #000;
+        font-family: verdana;
+    }
+    </style>
 @endsection
